7f607d99beca1d647eb59177cffcf425dfc2a2f1
Minyeong Kim <it@bakerylab.com.au>
2020-03-21 13:36:28 +1100

    week1 저장!

---
+++ week1/var_list.py
@@ -0,0 +1,30 @@
+# list -> 배열이라고 부른다
+# 파이썬에서는 리스트라고 부른다.
+# 리스트를 선언 할 때는 대괄호 열고 닫고 []
+# 같은 목적이 있는 값들을 모아놓을때 사용한다
+# 값에 접근 할 때는 동일하게 대괄호 [] -> 대괄호안에 몇번째 값을 볼 지 입
+# 배열의 맨앞은 0부터 시작한다
+
+# a = [1,2,3,4,5]
+# a[2] = 7
+# fruitList = ['apple','banana','pineapple']
+#
+# print(a[2])
+
+
+
+
+fruitList = ['orange','apple','banana','pineapple']
+fruitPriceList = [3000,2000,2500]
+
+print(fruitList[1])
+print(fruitPriceList[1])
+
+
+
+
+
+
+
+
+
